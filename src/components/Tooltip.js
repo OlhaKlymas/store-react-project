1145f378eac7cfc1d89a9abbfb87659517f36be9
OlhaKlymas <olhaklymas@gmail.com>
2023-04-22 05:10:36 +0300

    Updates

--- src/components/Tooltip.js
+++
@@ -1,9 +0,0 @@
-const Tooltip = (props) => {
-    return (
-        <div className='tooltip'>
-            <div className='tooltip__message'>{props.message}</div>
-        </div>
-    )
-}
-
-export default Tooltip;
--- src/pages/ErrorPage.js
+++
@@ -1,7 +0,0 @@
-const ErrorPage = (props) => {
-    return (
-        <div>{props.message}</div>
-    )
-}
-
-export default ErrorPage;
